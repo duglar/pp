ed3a3ea9f3cbc33a8437665a82f62579e26d4fe7
DUGLAR ZEMANATE <DUGLAR ZEMANATE@192.168.189.1>
2013-08-26 15:34:13 -0500

    se reliza el cambio del autor a duglar

---
+++ src/main/java/com/triunfo/daoimpl/CategoriaDAOIMPL.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.triunfo.daoimpl;
+
+/**
+ *
+ * @author DUGLAR ZEMANATE
+ */
+public class CategoriaDAOIMPL {
+    
+}
